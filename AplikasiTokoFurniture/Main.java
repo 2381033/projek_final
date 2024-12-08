2a344bf3d3f887317b39e6e1445c56bb23d74aef
Leon Simanjuntak <2381033@unai.edu>
2024-12-08 22:24:43 +0700

    update4

--- AplikasiTokoFurniture/Main.java
+++ AplikasiTokoFurniture/Main.java
@@ -1,13 +1,19 @@
 package AplikasiTokoFurniture;
 
+import AplikasiTokoFurniture.config.Database;
 import AplikasiTokoFurniture.entities.BarangFurniture;
+import AplikasiTokoFurniture.repositories.BarangFurnitureRepositoryDbImpl;
 import AplikasiTokoFurniture.services.BarangFurnitureService;
 import AplikasiTokoFurniture.services.BarangFurnitureServiceImpl;
 import AplikasiTokoFurniture.views.BarangFurnitureTerminalViewImpl;
 
+
 public class Main {
     public static void main(String[] args) {
-        BarangFurnitureService service = new BarangFurnitureServiceImpl();
+        Database database = new Database("furnituredb", "root", "", "localhost", "3306");
+        database.setup();
+        BarangFurnitureRepositoryDbImpl barangFurnitureRepositoryDb = new BarangFurnitureRepositoryDbImpl(database);
+        BarangFurnitureService service = new BarangFurnitureServiceImpl(barangFurnitureRepositoryDb);
         BarangFurnitureTerminalViewImpl view = new BarangFurnitureTerminalViewImpl();
 
         BarangFurniture barang = new BarangFurniture(1, "Kursi Kayu", 500000, "Kursi kayu berkualitas.");
--- module-info.java
+++ module-info.java
@@ -1,3 +1,12 @@
 module AplikasiTokoFurniture {
+    requires spring.context;
+    requires spring.beans;
     requires java.sql;
+    requires org.slf4j;
+
+    opens AplikasiTokoFurniture;
+    opens AplikasiTokoFurniture.entities;
+    opens AplikasiTokoFurniture.repositories;
+    opens AplikasiTokoFurniture.services;
+    opens AplikasiTokoFurniture.views;
 }
